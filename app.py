0e0d14615b2a21a380b09e1c724a47b7df49eca7
Akshay Shivaji Khade <168008184+akshaykhade-dev@users.noreply.github.com>
2026-05-09 23:41:22 +0000

    Converted  app to flask

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Hello DevOps CI/CD Pipeline")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello DevOps CI/CD Pipeline"
+
+app.run(host='0.0.0.0', port=5000)
